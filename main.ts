62865168b444954187e3f367488751ab90f734d1
veerababuteki <kalyan.u@iicl.in>
2025-08-11 12:06:21 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+bootstrapApplication(App, {
+  providers: [
+    importProvidersFrom(HttpClientModule)
+  ]
+});
